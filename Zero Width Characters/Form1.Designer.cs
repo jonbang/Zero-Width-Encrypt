5aa09ab6afbd1610aa6f62be2f29c72176857f8b
jonbang <31434698+jonbang@users.noreply.github.com>
2021-12-24 20:00:19 +1100

    Add files via upload

---
+++ Zero Width Characters/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace Zero_Width_Characters
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.HidePage = new System.Windows.Forms.TabPage();
+            this.RTEncode = new System.Windows.Forms.CheckBox();
+            this.StegOut = new System.Windows.Forms.TextBox();
+            this.PrivateMessageInput = new System.Windows.Forms.TextBox();
+            this.PublicMessageInput = new System.Windows.Forms.TextBox();
+            this.StegoText = new System.Windows.Forms.Label();
+            this.EncryptButton = new System.Windows.Forms.Button();
+            this.PrivateMessageInputLabel = new System.Windows.Forms.Label();
+            this.PublicMessageInputLabel = new System.Windows.Forms.Label();
+            this.RevealPage = new System.Windows.Forms.TabPage();
+            this.RevealMsg = new System.Windows.Forms.TextBox();
+            this.PubMsgDecode = new System.Windows.Forms.TextBox();
+            this.DecryptLabel = new System.Windows.Forms.Label();
+            this.EncryptLabel = new System.Windows.Forms.Label();
+            this.DecryptButton = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.HidePage.SuspendLayout();
+            this.RevealPage.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.HidePage);
+            this.tabControl1.Controls.Add(this.RevealPage);
+            this.tabControl1.Location = new System.Drawing.Point(0, 2);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1203, 692);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // HidePage
+            // 
+            this.HidePage.BackColor = System.Drawing.Color.Azure;
+            this.HidePage.Controls.Add(this.RTEncode);
+            this.HidePage.Controls.Add(this.StegOut);
+            this.HidePage.Controls.Add(this.PrivateMessageInput);
+            this.HidePage.Controls.Add(this.PublicMessageInput);
+            this.HidePage.Controls.Add(this.StegoText);
+            this.HidePage.Controls.Add(this.EncryptButton);
+            this.HidePage.Controls.Add(this.PrivateMessageInputLabel);
+            this.HidePage.Controls.Add(this.PublicMessageInputLabel);
+            this.HidePage.Location = new System.Drawing.Point(4, 29);
+            this.HidePage.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.HidePage.Name = "HidePage";
+            this.HidePage.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.HidePage.Size = new System.Drawing.Size(1195, 659);
+            this.HidePage.TabIndex = 0;
+            this.HidePage.Text = "Hide";
+            // 
+            // RTEncode
+            // 
+            this.RTEncode.AutoSize = true;
+            this.RTEncode.Checked = true;
+            this.RTEncode.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.RTEncode.Location = new System.Drawing.Point(76, 602);
+            this.RTEncode.Name = "RTEncode";
+            this.RTEncode.Size = new System.Drawing.Size(188, 24);
+            this.RTEncode.TabIndex = 10;
+            this.RTEncode.Text = "Real-Time-Encoding?";
+            this.RTEncode.UseVisualStyleBackColor = true;
+            // 
+            // StegOut
+            // 
+            this.StegOut.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.StegOut.Location = new System.Drawing.Point(12, 409);
+            this.StegOut.Multiline = true;
+            this.StegOut.Name = "StegOut";
+            this.StegOut.Size = new System.Drawing.Size(1162, 156);
+            this.StegOut.TabIndex = 9;
+            // 
+            // PrivateMessageInput
+            // 
+            this.PrivateMessageInput.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.PrivateMessageInput.Location = new System.Drawing.Point(12, 223);
+            this.PrivateMessageInput.Multiline = true;
+            this.PrivateMessageInput.Name = "PrivateMessageInput";
+            this.PrivateMessageInput.Size = new System.Drawing.Size(1162, 156);
+            this.PrivateMessageInput.TabIndex = 8;
+            this.PrivateMessageInput.TextChanged += new System.EventHandler(this.PrivateMessageInput_TextChanged);
+            // 
+            // PublicMessageInput
+            // 
+            this.PublicMessageInput.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.PublicMessageInput.Location = new System.Drawing.Point(12, 37);
+            this.PublicMessageInput.Multiline = true;
+            this.PublicMessageInput.Name = "PublicMessageInput";
+            this.PublicMessageInput.Size = new System.Drawing.Size(1162, 156);
+            this.PublicMessageInput.TabIndex = 7;
+            this.PublicMessageInput.TextChanged += new System.EventHandler(this.PublicMessageInput_TextChanged);
+            // 
+            // StegoText
+            // 
+            this.StegoText.AutoSize = true;
+            this.StegoText.Location = new System.Drawing.Point(9, 384);
+            this.StegoText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.StegoText.Name = "StegoText";
+            this.StegoText.Size = new System.Drawing.Size(178, 20);
+            this.StegoText.TabIndex = 6;
+            this.StegoText.Text = "Steganographized Text:";
+            // 
+            // EncryptButton
+            // 
+            this.EncryptButton.Location = new System.Drawing.Point(317, 585);
+            this.EncryptButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.EncryptButton.Name = "EncryptButton";
+            this.EncryptButton.Size = new System.Drawing.Size(578, 57);
+            this.EncryptButton.TabIndex = 4;
+            this.EncryptButton.Text = "Steganographize";
+            this.EncryptButton.UseVisualStyleBackColor = true;
+            this.EncryptButton.Click += new System.EventHandler(this.EncryptButton_Click);
+            // 
+            // PrivateMessageInputLabel
+            // 
+            this.PrivateMessageInputLabel.AutoSize = true;
+            this.PrivateMessageInputLabel.Location = new System.Drawing.Point(9, 198);
+            this.PrivateMessageInputLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.PrivateMessageInputLabel.Name = "PrivateMessageInputLabel";
+            this.PrivateMessageInputLabel.Size = new System.Drawing.Size(130, 20);
+            this.PrivateMessageInputLabel.TabIndex = 3;
+            this.PrivateMessageInputLabel.Text = "Private Message:";
+            // 
+            // PublicMessageInputLabel
+            // 
+            this.PublicMessageInputLabel.AutoSize = true;
+            this.PublicMessageInputLabel.Location = new System.Drawing.Point(14, 11);
+            this.PublicMessageInputLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.PublicMessageInputLabel.Name = "PublicMessageInputLabel";
+            this.PublicMessageInputLabel.Size = new System.Drawing.Size(124, 20);
+            this.PublicMessageInputLabel.TabIndex = 2;
+            this.PublicMessageInputLabel.Text = "Public Message:";
+            // 
+            // RevealPage
+            // 
+            this.RevealPage.BackColor = System.Drawing.Color.Azure;
+            this.RevealPage.Controls.Add(this.RevealMsg);
+            this.RevealPage.Controls.Add(this.PubMsgDecode);
+            this.RevealPage.Controls.Add(this.DecryptLabel);
+            this.RevealPage.Controls.Add(this.EncryptLabel);
+            this.RevealPage.Controls.Add(this.DecryptButton);
+            this.RevealPage.Location = new System.Drawing.Point(4, 29);
+            this.RevealPage.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.RevealPage.Name = "RevealPage";
+            this.RevealPage.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.RevealPage.Size = new System.Drawing.Size(1195, 659);
+            this.RevealPage.TabIndex = 1;
+            this.RevealPage.Text = "Reveal";
+            // 
+            // RevealMsg
+            // 
+            this.RevealMsg.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.RevealMsg.Location = new System.Drawing.Point(18, 307);
+            this.RevealMsg.Multiline = true;
+            this.RevealMsg.Name = "RevealMsg";
+            this.RevealMsg.Size = new System.Drawing.Size(1160, 235);
+            this.RevealMsg.TabIndex = 6;
+            // 
+            // PubMsgDecode
+            // 
+            this.PubMsgDecode.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.PubMsgDecode.Location = new System.Drawing.Point(18, 35);
+            this.PubMsgDecode.Multiline = true;
+            this.PubMsgDecode.Name = "PubMsgDecode";
+            this.PubMsgDecode.Size = new System.Drawing.Size(1160, 235);
+            this.PubMsgDecode.TabIndex = 5;
+            // 
+            // DecryptLabel
+            // 
+            this.DecryptLabel.AutoSize = true;
+            this.DecryptLabel.Location = new System.Drawing.Point(14, 284);
+            this.DecryptLabel.Name = "DecryptLabel";
+            this.DecryptLabel.Size = new System.Drawing.Size(155, 20);
+            this.DecryptLabel.TabIndex = 4;
+            this.DecryptLabel.Text = "Decrypted Message:";
+            // 
+            // EncryptLabel
+            // 
+            this.EncryptLabel.AutoSize = true;
+            this.EncryptLabel.Location = new System.Drawing.Point(15, 12);
+            this.EncryptLabel.Name = "EncryptLabel";
+            this.EncryptLabel.Size = new System.Drawing.Size(154, 20);
+            this.EncryptLabel.TabIndex = 3;
+            this.EncryptLabel.Text = "Encrypted Message:";
+            // 
+            // DecryptButton
+            // 
+            this.DecryptButton.Location = new System.Drawing.Point(460, 559);
+            this.DecryptButton.Name = "DecryptButton";
+            this.DecryptButton.Size = new System.Drawing.Size(322, 87);
+            this.DecryptButton.TabIndex = 1;
+            this.DecryptButton.Text = "De-Steganographize";
+            this.DecryptButton.UseVisualStyleBackColor = true;
+            this.DecryptButton.Click += new System.EventHandler(this.DecryptButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Zero-Width-Encrypter";
+            this.tabControl1.ResumeLayout(false);
+            this.HidePage.ResumeLayout(false);
+            this.HidePage.PerformLayout();
+            this.RevealPage.ResumeLayout(false);
+            this.RevealPage.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage HidePage;
+        private System.Windows.Forms.TabPage RevealPage;
+        private System.Windows.Forms.Label PublicMessageInputLabel;
+        private System.Windows.Forms.Label PrivateMessageInputLabel;
+        private System.Windows.Forms.Button EncryptButton;
+        private System.Windows.Forms.Button DecryptButton;
+        private System.Windows.Forms.Label StegoText;
+        private System.Windows.Forms.Label EncryptLabel;
+        private System.Windows.Forms.Label DecryptLabel;
+        private System.Windows.Forms.TextBox PubMsgDecode;
+        private System.Windows.Forms.TextBox RevealMsg;
+        private System.Windows.Forms.TextBox PublicMessageInput;
+        private System.Windows.Forms.TextBox StegOut;
+        private System.Windows.Forms.TextBox PrivateMessageInput;
+        private System.Windows.Forms.CheckBox RTEncode;
+    }
+}
+
---
+++ Zero Width Characters/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zero_Width_Characters
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static string Encrypt(string priv, string pub)
+        {
+            // Grab the public message string and break it up into characters
+            String[] PubMsg = pub.Split();
+
+            // Find the half-way point in the string
+            decimal length = pub.Length / 2;
+            int half = Convert.ToInt32(Math.Round(length, 0, MidpointRounding.AwayFromZero));
+            // Grab the private message
+            string PrivMsg = priv;
+
+            // Convert it to binary data
+            PrivMsg = str2bin(str2byte(PrivMsg, Encoding.UTF8));
+
+            // And convert that into a string of zero-width characters
+            PrivMsg = bin2hidden(PrivMsg);
+
+            // Finally, wrap it with a distinct boundary character
+            PrivMsg = wrap(PrivMsg);
+
+            // Inject the encoded private message into the approximate half-way point in the public string
+            int i = 0;
+            int v = 1;
+            List<string> Tmp = new List<string>();
+            List<char> Temp = new List<char>();
+            if (PubMsg.Length == 1)
+            {
+                Tmp.Add(PubMsg[0]);
+                Tmp.Add(PrivMsg);
+            }
+            else
+            {
+                v = 0;
+                foreach (char c in String.Join(" ", PubMsg))
+                {
+                    if (i == half)
+                    {
+                        Temp.AddRange(PrivMsg.ToCharArray());
+                    }
+                    Temp.Add(c);
+                    i++;
+                }
+            }
+            if (v == 1)
+            {
+                PubMsg = Tmp.ToArray();
+            }
+            else
+            {
+                string[] t = new[] { String.Concat(Temp) };
+                PubMsg = t;
+            }
+
+            // Display the public message with the hidden private message embedded
+            return String.Concat(PubMsg);
+        }
+
+        static string Decrypt(string pub)
+        {
+            // Unhide the message
+            string unwrapped = unwrap(pub);
+            string message = "";
+            // If it's not wrapped, process the full string as received
+            if (unwrapped == "false")
+            {
+                message = bin2str(hidden2bin(pub));
+            }
+            // Otherwise, process only the unwrapped string
+            else
+            {
+                message = bin2str(hidden2bin(unwrapped));
+            }
+
+            // Display the hidden private message
+            if (message.Length < 2)
+            {
+                return "No private message was found in that text.";
+            }
+            else
+            {
+                return message;
+            }
+        }
+
+        //Convert a String to a byte array
+        public static byte[] str2byte(string str, Encoding encoding)
+        {
+            return encoding.GetBytes(str);
+        }
+
+        //Convert a string to binary data
+        public static string str2bin(Byte[] text)
+        {
+            return string.Join(" ", text.Select(byt => Convert.ToString(byt, 2).PadLeft(8, '0')));
+        }
+
+        // Wrap a string with a distinct boundary
+        public static string wrap(string text)
+        {
+            return $"\uFEFF{text}\uFEFF"; // Unicode Character 'ZERO WIDTH NON-BREAKING SPACE' (U+FEFF) 0xEF 0xBB 0xBF
+        }
+
+        // Unwrap a string if the distinct boundary exists
+        public static string unwrap(string text)
+        {
+            String[] Tmp = text.Split("\uFEFF".ToCharArray());
+            if (Tmp.Length == 1) { return "false"; }
+            return Tmp[1];
+        }
+
+        // Convert binary data into a string
+        public static string bin2str(string bin)
+        {
+            bin = Regex.Replace(bin, "[^0-1]", " ");
+            String[] text = new string[] { };
+            List<Byte> va = new List<Byte>();
+            foreach(int vw in Enumerable.Range(0, bin.Split().Length))
+            {
+                va.Add(Convert.ToByte(Regex.Replace(bin.Split()[vw], @"\s", ""), 2));
+            }
+            return Encoding.UTF8.GetString(va.ToArray());
+        }
+
+        // Convert the ones, zeros, and spaces of the hidden binary data to their respective zero-width characters 
+        public static string bin2hidden(string text)
+        {
+            text = text.Replace(" ", "\u2060"); // Unicode Character 'WORD JOINER' (U+2060) 0xE2 0x81 0xA0
+            text = text.Replace("0", "\u200B"); // Unicode Character 'ZERO WIDTH SPACE' (U+200B) 0xE2 0x80 0x8B
+            text = text.Replace("1", "\u200C"); // Unicode Character 'ZERO WIDTH NON-JOINER' (U+200C) 0xE2 0x80 0x8C
+            return text;
+        }
+
+        // Convert zero-width characters to hidden binary data
+        public static string hidden2bin(string text)
+        {
+            text = text.Replace("\u2060", " "); // Unicode Character 'WORD JOINER' (U+2060) 0xE2 0x81 0xA0
+            text = text.Replace("\u200B", "0"); // Unicode Character 'ZERO WIDTH SPACE' (U+200B) 0xE2 0x80 0x8B
+            text = text.Replace("\u200C", "1"); // Unicode Character 'ZERO WIDTH NON-JOINER' (U+200C) 0xE2 0x80 0x8C
+            return text;
+        }
+
+        private void EncryptButton_Click(object sender, EventArgs e)
+        {
+            // Display the public message with the hidden private message embedded
+            StegOut.Text = Encrypt(PrivateMessageInput.Text, PublicMessageInput.Text);
+        }
+
+        private void DecryptButton_Click(object sender, EventArgs e)
+        {
+            // Display the private message hidden in the public message
+            RevealMsg.Text = Decrypt(PubMsgDecode.Text);
+        }
+
+        private void PublicMessageInput_TextChanged(object sender, EventArgs e)
+        {
+            if (RTEncode.Checked)
+            {
+                if (PrivateMessageInput.TextLength != 0)
+                {
+                    StegOut.Text = Encrypt(PrivateMessageInput.Text, PublicMessageInput.Text);
+                }
+            }
+        }
+
+        private void PrivateMessageInput_TextChanged(object sender, EventArgs e)
+        {
+            if (RTEncode.Checked)
+            {
+                if (PublicMessageInput.TextLength != 0)
+                {
+                    StegOut.Text = Encrypt(PrivateMessageInput.Text, PublicMessageInput.Text);
+                }
+            }
+        }
+    }
+}
